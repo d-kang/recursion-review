006281b0236a9db84fd24d1c73dc684bad2e073f
d-kang <david.kang714@gmail.com>
2017-07-03 13:03:26 -0700

    Handles Primitive Values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    obj = `[]`;
+  } else if (typeof obj === 'number') {
+    obj = `${obj}`;
+  } else if (obj === null) {
+    obj = 'null';
+  } else if (obj === true) {
+    obj = 'true';
+  } else if (obj === false) {
+    obj = 'false';
+  } else if (typeof obj === 'string') {
+    obj = `"${obj}"`;
+  }
+  return obj;
 };
+
+console.log(stringifyJSON(9));
+
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+];
