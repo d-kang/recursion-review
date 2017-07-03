06d7ffd20b2313e1fe92809c2af84c864538c29e
d-kang <david.kang714@gmail.com>
2017-07-03 15:52:44 -0700

    Can Parse Numbers and Null

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,59 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
 
+  var isNumber = Number(json);
+  if (isNumber === isNumber) {
+    return isNumber;
+  } else {
+    return null;
+  }
 };
+
+console.log(parseJSON('9'));
+console.log(parseJSON('null'));
+
+
+// var stringifiedObjects = [
+//   "9",
+//   "null",
+//   "true",
+//   "false",
+//   `"Hello world"`,
+//   "[]", "[8]",
+//   `["hi"]`,
+//   `[8,"hi"]`,
+//   "[1,0,-1,-0.3,0.3,1343.32,3345,0.00011999999999999999]",
+//   "[8,[[],3,4]]",
+//   `[[[["foo"]]]]`,
+//   "{}",
+//   `{"a":"apple"}`,
+//   `{"foo":true,"bar":false,"baz":null}`,
+//   `{"boolean, true":true,"boolean, false":false,"null":null}`,
+//   `{"a":{"b":"c"}}", "{"a":["b","c"]}`,
+//   `[{"a":"b"},{"c":"d"}]`,
+//   `{"a":[],"c":{},"b":true}`
+// ]
+
+// var stringifiableObjects = [
+//   9,
+//   null,
+//   true,
+//   false,
+//   'Hello world',
+//   [],
+//   [8],
+//   ['hi'],
+//   [8, 'hi'],
+//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+//   [8, [[], 3, 4]],
+//   [[[['foo']]]],
+//   {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+// ];
