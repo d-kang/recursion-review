341260274a8fbf219801263f77889425382933db
d-kang <david.kang714@gmail.com>
2017-07-03 11:39:28 -0700

    getElementsByClassName Outline

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  const nodeList = [];
+  const traverse = (className, $node) => {
+    if ($node.classList.contains(className)) {
+      nodeList.push($node);
+    }
+  };
+  traverse(className, document.body);
+  return nodeList;
 };
+
+console.log(getElementsByClassName('targetClassName'));
